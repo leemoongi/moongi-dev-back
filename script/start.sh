e06b61da414b223a2c76649311ed8c2ef3ee3538
MoonGi <01ki07@naver.com>
2024-01-09 10:16:54 +0900

    cicd test

--- script/start.sh
+++ script/start.sh
@@ -1,8 +1,3 @@
-
-echo "start.sh 시작" >> $DEPLOY_LOG
-echo "$rds_host" >> $DEPLOY_LOG
-echo "$rds_username" >> $DEPLOY_LOG
-
 PROJECT_ROOT="/home/ec2-user/moongi-dev-back"
 JAR_FILE="$PROJECT_ROOT/moongi-dev-back-0.0.1-SNAPSHOT.jar"
 
@@ -13,7 +8,12 @@ DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 TIME_NOW=$(date +%c)
 
 # jar 파일 실행
+echo "start.sh 시작" >> $DEPLOY_LOG
+echo "$rds_host" >> $DEPLOY_LOG
+echo "$rds_username" >> $DEPLOY_LOG
+
 echo "$TIME_NOW > $JAR_FILE --spring.profiles.active=dev 실행" >> $DEPLOY_LOG
+echo "java -jar $JAR_FILE --spring.profiles.active=dev"
 nohup java -jar $JAR_FILE --spring.profiles.active=dev > $APP_LOG 2> $ERROR_LOG &
 
 CURRENT_PID=$(pgrep -f $JAR_FILE)
