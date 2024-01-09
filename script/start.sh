e6aedc4346c49c530f60d6cbb9c6de4622b663f2
MoonGi <01ki07@naver.com>
2024-01-09 11:53:32 +0900

    cicd test

--- script/start.sh
+++ script/start.sh
@@ -9,14 +9,14 @@ TIME_NOW=$(date +%c)
 
 source ~/.bash_profile
 
-# jar 파일 실행
-echo "start.sh 시작" >> $DEPLOY_LOG
-echo "$rds_host" >> $DEPLOY_LOG
-echo "$rds_username" >> $DEPLOY_LOG
-
 echo "$TIME_NOW > $JAR_FILE --spring.profiles.active=dev 실행" >> $DEPLOY_LOG
 echo "java -jar $JAR_FILE --spring.profiles.active=dev"
 nohup java -jar $JAR_FILE --spring.profiles.active=dev > $APP_LOG 2> $ERROR_LOG &
 
+# 프로세스 아이디가 안나올때
+sleep 3
+
 CURRENT_PID=$(pgrep -f $JAR_FILE)
 echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
+
+echo "=========================================================================\n"
